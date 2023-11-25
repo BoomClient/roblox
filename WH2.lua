8d9f4a8c219d44e31dd9bddb75547914fa605e40
BoomClient <130541957+BoomClient@users.noreply.github.com>
2023-11-25 19:19:14 +0700

    Update WH2.lua

--- WH2.lua
+++ WH2.lua
@@ -1679,7 +1679,7 @@ local me = plrs.LocalPlayer;
 
     local function webHookOnEnd()
       if _G.webhookEnabled then
-        webhookLocals.timeLeft = convertToHMS(game.Players.LocalPlayer.Character.Name)
+        webhookLocals.timeLeft = game.Players.LocalPlayer.Character.Name
         webhookLocals.itemsEarned = {
           {
             color = '0';
